ea14e485d2ae2e10de29b9cfc5f56083b68fd703
leejonghyuk <as13ljh@gmail.com>
2023-04-10 10:53:22 +0900

    "Modify : token유무에 따라서 접속가능 페이지 수정"

--- src/Router.js
+++ src/Router.js
@@ -1,11 +1,13 @@
-
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import SignIn from './pages/SignIn/SignIn';
-import SignUp from './pages/SignUp/SignUp';
-import ToDo from './pages/ToDo/ToDo'
+import React from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+//import { BrowserRouter as Switch, Route, Redirect } from 'react-router-dom';
+import SignIn from "./pages/SignIn/SignIn";
+import SignUp from "./pages/SignUp/SignUp";
+import ToDo from "./pages/ToDo/ToDo";
 
 const Router = () => {
+  //const token = localStorage.getItem("token", undefined);
+
   return (
     <BrowserRouter>
       <Routes>
@@ -14,6 +16,20 @@ const Router = () => {
         <Route path="/todo" element={<ToDo />} />
       </Routes>
     </BrowserRouter>
+
+    // <BrowserRouter>
+    //   <Routes>
+    //     <Route
+    //       path="/signup"
+    //       element={token ? <Navigate to="/todo" /> : <SignUp />}
+    //     />
+    //     <Route
+    //       path="/"
+    //       element={token ? <Navigate to="/todo" /> : <SignIn />}
+    //     />
+    //     <Route path="/todo" element={token ? <ToDo /> : <Navigate to="/" />} />
+    //   </Routes>
+    // </BrowserRouter>
   );
 };
 
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,5 +1,102 @@
+import { useEffect } from 'react';
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./SignIn.scss";
 
-export default function Todo() {
-  return <p>signin</p>;
+export default function SignIn() {
+  const [userInfo, setUserInfo] = useState({ email: "", pw: "" });
+  const navigate = useNavigate();
+
+  const getUserInfo = (event) => {
+    const { name, value } = event.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      navigate("todo");
+    }
+  }, []);
+
+  const goToSignUp = () => {
+    navigate("/signup");
+  };
+
+  const goToToDo = () => {
+    fetch("https://www.pre-onboarding-selection-task.shop/auth/signin", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: userInfo.email,
+        password: userInfo.pw,
+      }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        localStorage.setItem("token", data.access_token);
+        if (data.access_token !== undefined) {
+          localStorage.setItem("token", data.access_token);
+          navigate("/todo");
+        } else {
+          alert("이이디 또는 비밀번호가 들렸습니다.");
+        }
+      });
+  };
+
+  const regex = /^\S+@\S+$/;
+
+  const isDisabled =
+    regex.test(userInfo.email) === true && userInfo.pw.length > 7;
+
+  return (
+    <div className="login">
+      <div className="header">로그인</div>
+      <section className="container">
+        <div className="loginGuide">
+          <span>이메일로 로그인해주세요.</span>
+        </div>
+        <div className="loginWrap">
+          <form>
+            <input
+              type="text"
+              className="inputEmail"
+              placeholder="이메일 입력"
+              onChange={getUserInfo}
+              name="email"
+              data-testid="email-input"
+            />
+            <input
+              type="password"
+              className="inputPw"
+              placeholder="비밀번호 입력 (8자이상)"
+              onChange={getUserInfo}
+              name="pw"
+              data-testid="password-input"
+            />
+            <button
+              type="button"
+              className={isDisabled ? "loginBtn" : "disabledBtn"}
+              onClick={goToToDo}
+              disabled={isDisabled ? false : true}
+              data-testid="signin-button"
+            >
+              로그인
+            </button>
+            <hr />
+          </form>
+          <button
+            onClick={goToSignUp}
+            className="joinMember"
+            data-testid="signup-button"
+          >
+            <span>아직 회원이 아니세요?</span>
+            <span> 회원가입</span>
+          </button>
+        </div>
+      </section>
+    </div>
+  );
 }
