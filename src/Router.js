1c464d15e591acd017edd2291b3ee83212507678
leejonghyuk <as13ljh@gmail.com>
2023-04-12 17:39:07 +0900

    "Modify : Router.js 수정"

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
-//import { BrowserRouter as Switch, Route, Redirect } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./pages/SignIn/SignIn";
 import SignUp from "./pages/SignUp/SignUp";
 import Todo from "./pages/Todo/Todo";
 
 const Router = () => {
-  //const token = localStorage.getItem("token", undefined);
-
   return (
     <BrowserRouter>
       <Routes>
@@ -16,20 +13,6 @@ const Router = () => {
         <Route path="/todo" element={<Todo />} />
       </Routes>
     </BrowserRouter>
-
-    // <BrowserRouter>
-    //   <Routes>
-    //     <Route
-    //       path="/signup"
-    //       element={token ? <Navigate to="/todo" /> : <SignUp />}
-    //     />
-    //     <Route
-    //       path="/"
-    //       element={token ? <Navigate to="/todo" /> : <SignIn />}
-    //     />
-    //     <Route path="/todo" element={token ? <ToDo /> : <Navigate to="/" />} />
-    //   </Routes>
-    // </BrowserRouter>
   );
 };
 
